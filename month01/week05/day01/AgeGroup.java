01f43b2d20f4335c8f033abf11753a32dbe46360
Zaya <sainzayaz65@gmail.com>
2026-04-28 17:43:38 +0800

    b

---
+++ month01/week05/day01/AgeGroup.java
@@ -0,0 +1,22 @@
+package nippon_spring_2026.month01.week05.day01;
+
+import java.util.Scanner;
+
+public class AgeGroup {
+    public static void main(String[] args) {
+        Scanner agegroupScanner = new Scanner(System.in);
+        System.out.println("Nasaa oruulna uu:");
+        int age = agegroupScanner.nextInt();
+        if (age <= 2) {
+            System.out.println("Nylh");
+        } else if (age <= 12) {
+            System.out.println("Huuhed");
+        } else if (age <= 17) {
+            System.out.println("Usvur nas");
+        } else if (age <= 64) {
+            System.out.println("Nasand hursen");
+        } else {
+            System.out.println("Ahmad");
+        }
+    }
+}
---
+++ month01/week05/day01/TrafficLight.java
@@ -0,0 +1,25 @@
+package nippon_spring_2026.month01.week05.day01;
+
+import java.util.Scanner;
+
+public class TrafficLight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Gerliin ungiig oruulna uu (red/yellow/green): ");
+        String light = sc.next().toLowerCase();
+
+        switch (light) {
+            case "red":
+                System.out.println("Zogs!");
+                break;
+            case "yellow":
+                System.out.println("Belen bai!");
+                break;
+            case "green":
+                System.out.println("Yv!");
+            default:
+                System.out.println("Buruu ungu! ");
+        }
+
+    }
+}
