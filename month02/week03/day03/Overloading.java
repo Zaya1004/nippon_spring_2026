521339865ec37b50a5c00ca8b00a4e4569e171d2
Zaya <sainzayaz65@gmail.com>
2026-05-13 18:00:32 +0800

    n

---
+++ month02/week03/day03/Overloading.java
@@ -0,0 +1,26 @@
+package month02.week03.day03;
+
+public class Overloading {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static double add(double a, double b) {
+        return a + b;
+    }
+
+    static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    static String add(String a, String b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(3, 5));
+        System.out.println(add(3.14, 2.71));
+        System.out.println(add(1, 2, 3));
+        System.out.println(add("java", "dev"));
+    }
+}
